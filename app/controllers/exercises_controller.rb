216e5e4bf373805a5e9a5ca00250fb9890369002
codymerritt347 <cody.merritt@icloud.com>
2021-06-24 12:40:54 -0400

    Edit index render json

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,19 +1,16 @@
 class ExercisesController < ApplicationController
   before_action :set_exercise, only: [:show, :update, :destroy]
 
-  # GET /exercises
   def index
     @exercises = Exercise.all
 
     render json: @exercises
   end
 
-  # GET /exercises/1
   def show
     render json: @exercise
   end
 
-  # POST /exercises
   def create
     @exercise = Exercise.new(exercise_params)
 
@@ -24,7 +21,6 @@ class ExercisesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /exercises/1
   def update
     if @exercise.update(exercise_params)
       render json: @exercise
@@ -33,18 +29,15 @@ class ExercisesController < ApplicationController
     end
   end
 
-  # DELETE /exercises/1
   def destroy
     @exercise.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_exercise
       @exercise = Exercise.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def exercise_params
       params.require(:exercise).permit(:name, :target, :equipment, :workout_id)
     end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,19 +1,16 @@
 class WorkoutsController < ApplicationController
   before_action :set_workout, only: [:show, :update, :destroy]
 
-  # GET /workouts
   def index
     @workouts = Workout.all
 
-    render json: @workouts
+    render json: @workouts, only: [:id, :name, :created_at], include: {exercises: {except: [:id, :created_at, :updated_at]}}
   end
 
-  # GET /workouts/1
   def show
     render json: @workout
   end
 
-  # POST /workouts
   def create
     @workout = Workout.new(workout_params)
 
@@ -24,7 +21,6 @@ class WorkoutsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /workouts/1
   def update
     if @workout.update(workout_params)
       render json: @workout
@@ -33,18 +29,15 @@ class WorkoutsController < ApplicationController
     end
   end
 
-  # DELETE /workouts/1
   def destroy
     @workout.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_workout
       @workout = Workout.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def workout_params
       params.require(:workout).permit(:name)
     end
